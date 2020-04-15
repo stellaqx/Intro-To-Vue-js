805fc21c30da1d4db97ead90f472ecaa898f434c
Qian <ee.xuqian@gmail.com>
2020-04-15 00:47:57 -0700

    vue event handling.

--- main.js
+++ main.js
@@ -11,16 +11,30 @@ var app = new Vue({
             {
                 variantId: 2234, 
                 variantColor: "green",
+                variantImage: './assets/vmSocks-green-onWhite.jpg',
             },
             {
                 variantId: 2235, 
                 variantColor: "blue",
+                variantImage: './assets/vmSocks-blue-onWhite.jpg',
             },
         ],
+        cart: 0,
         
         url: 'https://www.w3schools.com/tags/att_a_href.asp', 
         description: 'This is a detailed description.',
         onSale: true,
         sizes: ['small', 'medium', 'large'],
+    },
+    methods: {
+        addToCart: function () {
+            this.cart += 1; 
+        },
+        updateProduct: function (variantImage) {
+            this.image = variantImage;
+        },
+        removeFromCart: function () {
+            this.cart -= 1;
+        },
     }
 });
