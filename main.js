e804b057868cabbe6fc9c12b868e0735677c3470
Qian <ee.xuqian@gmail.com>
2020-04-15 01:13:07 -0700

    computed properties.

--- main.js
+++ main.js
@@ -3,20 +3,22 @@ Vue.config.devtools = true;
 var app = new Vue({
     el: "#app", 
     data: {
+        brand: 'My Brand',
         product: 'Socks',
-        image: './assets/vmSocks-green-onWhite.jpg',
-        inStock: false,
+        selectedVariant: 0,
         details:["95% cotton", "5% plyester", "gender-neural"],
         variants: [
             {
                 variantId: 2234, 
                 variantColor: "green",
                 variantImage: './assets/vmSocks-green-onWhite.jpg',
+                variantQuantity: 10,
             },
             {
                 variantId: 2235, 
                 variantColor: "blue",
                 variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                variantQuantity: 0,
             },
         ],
         cart: 0,
@@ -30,11 +32,27 @@ var app = new Vue({
         addToCart: function () {
             this.cart += 1; 
         },
-        updateProduct: function (variantImage) {
-            this.image = variantImage;
+        updateProduct: function (index) {
+            this.selectedVariant = index;
+            // console.log(index);
         },
         removeFromCart: function () {
             this.cart -= 1;
         },
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product; 
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity > 0;
+        },
+        onSaleMsg() {
+            if (this.onSale) return this.brand + ' ' + this.product + ' is on sale.';
+            return '';
+        }
     }
 });
