153459fc4649c91f9867b4d4b02ded7f7754ccc5
Qian <ee.xuqian@gmail.com>
2020-04-15 00:30:31 -0700

    vue conditional display.

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ var app = new Vue({
     el: "#app", 
     data: {
         product: 'Socks',
-        description: 'This is a detailed description.',
         image: './assets/vmSocks-green-onWhite.jpg',
+        inStock: true,
+
         url: 'https://www.w3schools.com/tags/att_a_href.asp', 
+        description: 'This is a detailed description.',
+        onSale: true,
     }
 });
