c118b5c59ef20ad6b8900a34f0b79f3fa038da15
Qian <ee.xuqian@gmail.com>
2020-04-15 00:37:31 -0700

    vue list rendering

--- main.js
+++ main.js
@@ -6,9 +6,21 @@ var app = new Vue({
         product: 'Socks',
         image: './assets/vmSocks-green-onWhite.jpg',
         inStock: true,
-
+        details:["95% cotton", "5% plyester", "gender-neural"],
+        variants: [
+            {
+                variantId: 2234, 
+                variantColor: "green",
+            },
+            {
+                variantId: 2235, 
+                variantColor: "blue",
+            },
+        ],
+        
         url: 'https://www.w3schools.com/tags/att_a_href.asp', 
         description: 'This is a detailed description.',
         onSale: true,
+        sizes: ['small', 'medium', 'large'],
     }
 });
