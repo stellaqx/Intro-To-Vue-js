f375a9ef93008396fe7a374af0ad7226e7bd58ab
Qian <ee.xuqian@gmail.com>
2020-04-15 00:23:57 -0700

    Using v-bind to dynamically bind attributes.

--- main.js
+++ main.js
@@ -5,5 +5,7 @@ var app = new Vue({
     data: {
         product: 'Socks',
         description: 'This is a detailed description.',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        url: 'https://www.w3schools.com/tags/att_a_href.asp', 
     }
 });
